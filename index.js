dc2a612f07d181a278885e0ec26def18c421d5f6
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-02 00:14:33 -0500

    Creado el index, con las pruebas de que mongoose puede operar crud de un modelo asignado

---
+++ index.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Dishes = require("./models/dishes");
+
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+
+  const dish = Dishes({
+    name: "Uthappizza",
+    description: "test",
+  });
+
+  dish
+    .save()
+    .then((dish) => {
+      console.log(dish);
+
+      return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+      console.log(dishes);
+
+      return Dishes.deleteOne({});
+    })
+    .then(() => {
+      return mongoose.connection.close();
+    })
+    .catch((error) => console.log(error));
+});
